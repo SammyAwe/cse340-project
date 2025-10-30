7e455f44a38268025a0cd2da68516858810bfdb5
SammyAwe <awesamuelopeoluwa@gmail.com>
2025-10-30 10:59:57 -0700

    server .js ready for render

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const path = require("path")
-const app = express()
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout")
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-const staticRoutes = require("./routes/static")
-app.use(staticRoutes)
+const staticRoutes = require("./routes/static");
+app.use(staticRoutes);
 
 
 app.get("/", (req, res) => {
-  res.render("index", {title: "Home" })
-})
+  res.render("index", {title: "Home" });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
-})
+});
